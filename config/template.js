0e3f2a278068208ffc0ab3a99605e254cce863df
BenchiBench <38825061+BenchiBench@users.noreply.github.com>
2026-02-15 17:28:20 +0800

    更新 template.js

--- config/template.js
+++ config/template.js
@@ -16,9 +16,9 @@ udp-relay = true
 function buildProxyGroup(proxyNames) {
   return `[Proxy Group]
 🚀 节点选择 = select, ${proxyNames.join(', ')}, 🌏 自动选择, 🔄 故障切换, DIRECT
-🌏 自动选择 = url-test, ${proxyNames.join(', ')}, url=http://www.gstatic.com/generate_204, interval=300
+🌏 自动选择 = url-test, ${proxyNames.join(', ')}, url=http://www.gstatic.com/generate_204, interval=300, tolerance=50
 🔄 故障切换 = fallback, ${proxyNames.join(', ')}, url=http://www.gstatic.com/generate_204, interval=180
-`;
+🚫 广告拦截 = select, REJECT, DIRECT`;
 }
 
 const ruleBlock =
