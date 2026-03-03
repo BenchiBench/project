a4aff14bb70fc3180e0d8065edb652d0e447848e
BenchiBench <38825061+BenchiBench@users.noreply.github.com>
2026-03-03 21:02:48 +0800

    更新 trojan.js

--- protocols/trojan.js
+++ protocols/trojan.js
@@ -3,14 +3,8 @@ const { appendIf } = require('../utils');
 function parse(line) {
   try {
     const u = new URL(line);
-    const name = decodeURIComponent(u.hash.slice(1) || 'trojan').trim();
-
-    const out = [
-      `${name} = trojan`,
-      u.hostname,
-      u.port,
-      `password=${decodeURIComponent(u.username)}`
-    ];
+    const name = decodeURIComponent(u.hash.slice(1)) || 'node';
+    const out = [`${name} = trojan`, u.hostname, u.port, `password=${decodeURIComponent(u.username)}`];
 
     appendIf(out, 'sni', u.searchParams.get('peer') || u.searchParams.get('sni'));
     appendIf(out, 'alpn', u.searchParams.get('alpn'));
@@ -22,7 +16,4 @@ function parse(line) {
   }
 }
 
-module.exports = {
-  prefixes: ['trojan://'],
-  parse
-};
+module.exports = { prefixes: ['trojan://'], parse };
