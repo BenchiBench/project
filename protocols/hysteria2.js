a2c1a6f619515472a4c98a3214726ed5816c0bb4
BenchiBench <38825061+BenchiBench@users.noreply.github.com>
2026-03-03 10:50:39 +0800

    更新 hysteria2.js

--- protocols/hysteria2.js
+++ protocols/hysteria2.js
@@ -1,11 +1,11 @@
-const { cleanName, appendIf } = require('../utils');
+const { appendIf } = require('../utils');
 
 function parse(line) {
   try {
     line = line.replace('hy2://', 'hysteria2://');
     const u = new URL(line);
+    const name = (decodeURIComponent(u.hash.slice(1)) || 'hysteria2').replace(/[\r\n\t]/g, '').trim();
 
-    const name = cleanName(decodeURIComponent(u.hash.slice(1)));
     const out = [
       `${name} = hysteria2`,
       u.hostname,
@@ -14,16 +14,13 @@ function parse(line) {
     ];
 
     const params = u.searchParams;
-
     appendIf(out, 'sni', params.get('sni'));
     appendIf(out, 'alpn', params.get('alpn'));
     appendIf(out, 'obfs', params.get('obfs'));
     appendIf(out, 'obfs-password', params.get('obfs-password'));
     appendIf(out, 'up', params.get('up'));
     appendIf(out, 'down', params.get('down'));
-    appendIf(out, 'skip-cert-verify',
-      params.get('insecure') === '1' ? 'true' : 'false'
-    );
+    appendIf(out, 'skip-cert-verify', params.get('insecure') === '1' ? 'true' : 'false');
 
     return out.join(', ');
   } catch {
